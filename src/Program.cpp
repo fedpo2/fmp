e8025665c2c72be1e1d841669d219f47f2aead54
fede <federico.nicolas.polidoro@gmail.com>
2023-03-18 00:21:33 -0300

    Cambios a ser confirmados: 	modificados:     src/Program.cpp  Cambios no rastreados para el commit: 	modificados:     makefile  Archivos sin seguimiento: 	.vscode/ 	bin/

--- src/Program.cpp
+++ src/Program.cpp
@@ -4,14 +4,32 @@
 //=================================//
 //  ./music [cancion1][cancion2].. //
 //=================================//
+//
+//  pause function needs to be reworked
+//  
+
 
 using std::string;
+
+void passSong(int *selector, Music *music, char **argv[]) {
+  *selector--;
+  *music = LoadMusicStream(*argv[*selector]);
+  PlayMusicStream(*music);
+}
 
 int main (int argc, char *argv[]) {
 
   int selector = 0;
   bool pause;
   float timePlayed = 0.0f;
+  
+  if (argc<2) {
+    std::cout << "      FMP - Fede Music Player\n"
+              << "//=================================// \n" 
+              << "//  ./music [cancion1][cancion2].. // \n"
+              << "//=================================// \n\n"; 
+    return 0;
+  }
 
   InitWindow(400,200,"fede");
   SetTargetFPS(60);
@@ -58,13 +76,18 @@ int main (int argc, char *argv[]) {
       timePlayed = GetMusicTimePlayed(music)/GetMusicTimeLength(music);
 
       //chequea que el tiempo de reproduccion de la musica no sea más larga que la cancion 
-      if (timePlayed > 1.0f) timePlayed = 1.0f;
-
+      if (timePlayed > 1.0f) {
+        timePlayed = 1.0f;
+        
+        if (selector != argc-1) {
+          passSong(&selector, &music, &argv);
+        }
+      }
       BeginDrawing();
 
         ClearBackground(RAYWHITE);
         DrawText("q = salir\np = pause/resume\nr = reset song\n<-/-> = change song", 20, 48, 20, DARKPURPLE);
-        
+        DrawText(argv[selector], 20, 180, 8, BLUE);
         DrawRectangle(20, 20, 360, 12, LIGHTGRAY);
         DrawRectangle(20, 20, (int)(timePlayed*360.0f), 12, MAROON);
         
@@ -76,6 +99,12 @@ int main (int argc, char *argv[]) {
   CloseWindow();
   return 0;
 }
+//by fede
+
+
+
+
+
 
 /*
 char[] <- campo 
